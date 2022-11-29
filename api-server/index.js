a8eb0fd9e8e40a91f480bb869f62f3cb83bc8f82
Sheeva Haghighat <h.sheeva@gmail.com>
2022-11-28 19:35:39 -0800

    add initial routes

--- api-server/index.js
+++ api-server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const axios = require('axios');
 const express = require('express');
 const logger = require("./middleware/logger");
+const controller = require('./controller/helper.js');
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +11,18 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded());
 //app.use(logger);
+//app.get('/products/:product_id', getProductHandler);
+
+
+app.get('/qa/questions');
+app.get('/qa/questions/:question_id/answers');
+app.post('/qa/questions');
+app.post('/qa/questions/:question_id/answers');
+app.put('/qa/questions/:question_id/helpful');
+app.put('/qa/questions/:question_id/report');
+app.put('/qa/answers/:answer_id/helpful');
+app.put('/qa/answers/:answer_id/report');
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
